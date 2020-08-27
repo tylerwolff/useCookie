0b239d4d2cfca5a36d450743573ee3e69f22b4ca
Marco Casula <mc@clickbit.net>
2020-08-27 10:13:30 -0600

    impl hasJsonStructure function and relative tests
    the function comes from https://stackoverflow.com/questions/9804777/how-to-test-if-a-string-is-json-or-not

--- index.js
+++ index.js
@@ -27,6 +27,18 @@ export const setCookie = (name, value, options) => {
     optionsWithDefaults.path;
 };
 
+export const hasJsonStructure = (str) => {
+  if (typeof str !== 'string') return false;
+  try {
+      const result = JSON.parse(str);
+      const type = Object.prototype.toString.call(result);
+      return type === '[object Object]'
+          || type === '[object Array]';
+  } catch (err) {
+      return false;
+  }
+}
+
 export const getCookie = (name) => {
   return document.cookie.split('; ').reduce((r, v) => {
     const parts = v.split('=');
